3a92945238078f97bbe595f665ea66de273ebb8e
suren <surenanthonyvithanage@gmail.com>
2018-09-29 00:42:54 +0530

    Changes 29/9/2018

--- app/src/main/java/com/example/krishna/codetalkers/DatabaseHelper.java
+++ app/src/main/java/com/example/krishna/codetalkers/DatabaseHelper.java
@@ -10,13 +10,20 @@ import android.widget.Toast;
 import com.example.krishna.codetalkers.columNames.stockTable;
 
 import java.sql.Blob;
-
-
+import java.util.ArrayList;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String Database_Name = "Store.db";
     public static final String Cart_Table_Name = "cart";
+
+    //Store
+    private static final String TABLE_NAME = "item";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_NAME = "name";
+    private static final String COLUMN_SECTION = "section";
+    private static final String COLUMN_JOIN_DATE = "joiningdate";
+    private static final String COLUMN_PRICE = "price";
 
 
     private static final String User_TABLE_CREATE =
@@ -28,11 +35,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                     columNames.userTable.User_Password + " TEXT, " + columNames.userTable.User_Mobile + " Integer)";
 
     private static final String Stock_TABLE_CREATE =
-            "CREATE TABLE " + stockTable.Stock_Name + " (" +
-                    stockTable.Stock_Id + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    stockTable.Stock_Name + " TEXT, " +
-                    stockTable.Stock_Description + " TEXT, " +
-                    stockTable.Stock_Price + " TEXT)";
+            "CREATE TABLE " + columNames.stockTable.Stock_Table+ " (" +
+                    columNames.stockTable.Stock_Id + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    columNames.stockTable.Stock_Name + " TEXT, " +
+                    columNames.stockTable.Stock_Description + " TEXT, " +
+                    columNames.stockTable.Stock_Price + " INTEGER )";
+
+    //Store
+//    String sql = "CREATE TABLE " + TABLE_NAME + " (\n" +
+//            "    " + COLUMN_ID + " INTEGER NOT NULL CONSTRAINT item_pk PRIMARY KEY AUTOINCREMENT,\n" +
+//            "    " + COLUMN_NAME + " varchar(200) NOT NULL,\n" +
+//            "    " + COLUMN_SECTION + " varchar(200) NOT NULL,\n" +
+//            "    " + COLUMN_JOIN_DATE + " datetime NOT NULL,\n" +
+//            "    " + COLUMN_PRICE + " double NOT NULL\n" +
+//            ");";
+
+    String sql = "create table item ( id INTEGER NOT NULL CONSTRAINT item_pk PRIMARY KEY AUTOINCREMENT ,name varchar(200) NOT NULL, section varchar(200) NOT NULL , joiningdate datetime NOT NULL , price NOT NULL )";
 
 
     public DatabaseHelper(Context context) {
@@ -44,9 +62,12 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         //Stock Table
         db.execSQL(Stock_TABLE_CREATE);
-
+        //Item Table
+        db.execSQL(sql);
         //User Table
         db.execSQL(User_TABLE_CREATE);
+
+
 
 
     }
@@ -56,7 +77,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
         db.execSQL("DROP TABLE IF EXISTS " + stockTable.Stock_Table);
         db.execSQL("DROP TABLE IF EXISTS " + columNames.userTable.User_Table);
+        String sql = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
+        db.execSQL(sql);
         onCreate(db);
+
 
     }
 
@@ -84,7 +108,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         contentValues.put(columNames.stockTable.Stock_Price, price);
         //contentValues.put(columNames.stockTable.Stock_Photo, pic);
 
-        long result = db.insert(columNames.userTable.User_Table, null, contentValues);
+        long result = db.insert(columNames.stockTable.Stock_Table, null, contentValues);
 
         if (result == -1)
             return false;
@@ -103,6 +127,58 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         } else {
             return false;
         }
+    }
+
+    public columNames getUserDetails(String id) {
+        // get readable database as we are not inserting anything
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("select * from user where uid = ?",new String[]{id});
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        columNames u = new columNames(
+                cursor.getString(cursor.getColumnIndex(columNames.userTable.User_Name)),
+                cursor.getString(cursor.getColumnIndex(columNames.userTable.User_email)),
+                cursor.getString(cursor.getColumnIndex(columNames.userTable.User_Address)),
+                cursor.getString(cursor.getColumnIndex(columNames.userTable.User_Mobile)));
+
+        // close the db connection
+        cursor.close();
+
+        return u;
+    }
+
+
+    //Store
+    boolean addItem(String name, String section, String joiningdate, double price) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_NAME, name);
+        contentValues.put(COLUMN_SECTION, section);
+        contentValues.put(COLUMN_JOIN_DATE, joiningdate);
+        contentValues.put(COLUMN_PRICE, price);
+        SQLiteDatabase db = getWritableDatabase();
+        return db.insert("item", null, contentValues) != -1;
+    }
+
+    Cursor getAllItem() {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+    }
+
+    boolean updateItem(int id, String name, String section, double price) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COLUMN_NAME, name);
+        contentValues.put(COLUMN_SECTION, section);
+        contentValues.put(COLUMN_PRICE, price);
+        return db.update(TABLE_NAME, contentValues, COLUMN_ID + "=?", new String[]{String.valueOf(id)}) == 1;
+    }
+
+    boolean deleteItem(int id) {
+        SQLiteDatabase db = getWritableDatabase();
+        return db.delete(TABLE_NAME, COLUMN_ID + "=?", new String[]{String.valueOf(id)}) == 1;
     }
 }
 
